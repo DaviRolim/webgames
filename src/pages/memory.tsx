0df691e2a61f9d37cb6de0d253ec811af812f7c1
Davi Rolim <davirolim94@gmail.com>
2022-12-12 08:28:52 -0300

    adding head and background to memory page

--- src/pages/memory.tsx
+++ src/pages/memory.tsx
@@ -1,8 +1,20 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import MatchPairsBoard from "../modules/memory/MatchPairsBoard";
+import styles from "./index.module.css";
 
 const MatchPairsGame: NextPage = () => {
-  return <MatchPairsBoard />;
+  return (
+    <>
+      <Head>
+        <title>Match Pairs</title>
+        <link rel="icon" href="/images/av-azul.png" />
+      </Head>
+      <main className={styles.main}>
+        <MatchPairsBoard />
+      </main>
+    </>
+  );
 };
 
 export default MatchPairsGame;
